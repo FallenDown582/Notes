29fec5e46588c4a502e38c81e68981df63f61935
Reggie Adams <reggieadams5802@gmail.com>
2019-04-23 14:13:25 -0500

    Commit for DS store file

--- 22.matrices/MatrixSearch.java
+++ 22.matrices/MatrixSearch.java
@@ -7,18 +7,25 @@ import static java.lang.System.*;
 
 public class MatrixSearch
 {
-	/*
-	 *  method searchMat should determine how many
-	 *  occurrences of toFind exist in the matrix mat
-	 */
-	public static int searchMat( int[][] mat, int toFind)
-	{
-		return 0;
-	}
+ /*
+  *  method searchMat should determine how many
+  *  occurrences of toFind exist in the matrix mat
+  */
+ public static int searchMat( int[][] mat, int toFind)
+ {
+   int count = 0;
+   for(int[] ray : mat){
+     for(int elem : ray){
+        if(elem == toFind)
+          count += 1;
+     }
+   }
+   return count;
+ }
 
-	public static void main(String args[])
-	{
-		int[][] twoDRay = {{5,7,7,8,7,8},{5,3,4,6},{0,8,9,1,2,3,4,5}};
-		System.out.println( searchMat( twoDRay, 7) );
-	}
+ public static void main(String args[])
+ {
+  int[][] twoDRay = {{5,7,7,8,7,8},{5,3,4,6},{0,8,7,1,2,3,4,5}};
+  System.out.println( searchMat( twoDRay, 7) );
+ }
 }
--- 22.matrices/MatrixSetTwo.java
+++ 22.matrices/MatrixSetTwo.java
@@ -7,26 +7,27 @@ import static java.lang.System.*;
 
 public class MatrixSetTwo
 {
-	public static void main(String args[])
-	{
-		int[][] mat = new int[5][5];
+ public static void main(String args[])
+ {
+  int[][] mat = new int[5][5];
 
-		for(int r=0; r<mat.length; r++)
-		{
-			for(int c=0; c<mat[0].length; c++)
-			{
-				mat[r][c] = r*c;
-			}
-		}
+  for(int r=0; r<mat.length; r++)
+  {
+   for(int c=0; c<mat[0].length; c++)
+   {
+    if(r==c)
+      mat[r][c] = r*c;
+   }
+  }
 
-		for(int r=0; r<mat.length; r++)
-		{
-			out.print("row "+ r + " ");
-			for(int c=0; c<mat[0].length; c++)
-			{
-				out.printf("%3d",mat[r][c]);
-			}
-			out.println();
-		}
-	}
+  for(int r=0; r<mat.length; r++)
+  {
+   out.print("row "+ r + " ");
+   for(int c=0; c<mat[0].length; c++)
+   {
+    out.printf("%3d",mat[r][c]);
+   }
+   out.println();
+  }
+ }
 }
